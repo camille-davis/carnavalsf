7f1065fe65e63f0c37462ffc41a37e9580d3ba94
Camille Davis <camilledavisdev@gmail.com>
2025-10-29 11:59:07 -0700

    Add logo support.

--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@
  * CarnavalSF Theme Functions
  */
 
+// Add theme support for website logo.
+add_theme_support( 'custom-logo' );
+
 // Register navigation menus
 function carnavalsf_register_menus() {
   register_nav_menus([
