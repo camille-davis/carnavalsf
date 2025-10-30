5310714975fbf78a6b04dc32471643a49031f223
Camille Davis <camilledavisdev@gmail.com>
2025-10-29 22:14:47 -0700

    Update page template and style, column and details blocks.

--- page.php
+++ page.php
@@ -1,19 +1,22 @@
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 
 <main id="primary" class="site-main">
-  <?php
-  while (have_posts()) : the_post();
-    get_template_part('template-parts/content', 'page');
-    
-    if (comments_open() || get_comments_number()) :
-      comments_template();
-    endif;
-  endwhile;
-  ?>
+  <?php while (have_posts()) : the_post(); ?>
+  <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <!-- Featured image -->
+    <?php if (has_post_thumbnail()) : ?>
+      <div class="featured-image" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
+      </div>
+    <?php endif; ?>
+
+    <div class="entry-content">
+      <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+      <?php the_content(); ?>
+    </div>
+  </div>
+  <?php endwhile; ?>
 </main>
 
 <?php
-get_sidebar();
 get_footer();
